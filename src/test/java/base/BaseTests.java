6a06d86fc04d9bdab311ed99f609d6233cc13436
Audrey Era Goldenia <audrey.goldenia@aux.dkatalis.com>
2023-02-13 17:10:19 +0700

    adding login test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,51 +1,30 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.HomePage;
 
-import java.sql.SQLOutput;
-import java.util.List;
 
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homepage;
 
+    @BeforeClass
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
         homepage = new HomePage(driver);
-
-        //Exercise Chapter 3
-        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContentLink.click();
-
-        WebElement exampleOneLink = driver.findElement(By.linkText("Example 1: Menu Element"));
-        exampleOneLink.click();
-
-        List<WebElement> menuElement = driver.findElements(By.tagName("li"));
-        System.out.println("Menu Element Size: " + menuElement.size());
-
-        WebElement homeLink = driver.findElement(By.linkText("Home"));
-        homeLink.click();
-
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println("Home Link Size: " + links.size());
-
-        WebElement checkboxesLink = driver.findElement(By.linkText("Checkboxes"));
-        checkboxesLink.click();
-
-        System.out.println(driver.getTitle());
-
     }
 
-    public static void main(String args[]){
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
     }
+
 }
